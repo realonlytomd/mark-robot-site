50beee01c66e6f87dd6b41b19fc2a966302eec48
Tom McLaughlin <mclaughlintomd@gmail.com>
2023-09-01 13:15:22 -0500

    starting marks

---
+++ src/Landing.js
@@ -0,0 +1,17 @@
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div className="Landing"> 
+        <header className="Landing-header">
+            <div className="Justwords">
+                <p>
+                Mark's info goes here.
+                </p>
+            </div>
+        </header> 
+    </div>
+  );
+}
+
+export default Landing;
