b9b1dc271ce09da2c81d6bd3d3c39e44ad78ce31
realonlytomd <mclaughlintomd@gmail.com>
2023-11-13 17:25:47 -0600

    changed App file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import Landing from './Landing';
 import Pictures from './Pictures';
 import Profiles from './Profiles';
@@ -9,11 +10,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Landing />
-    <Pictures />
-    <Profiles />
+    <App />
   </React.StrictMode>
 );
+// above between React.StrictMode code, Put: <Landing />  <Pictures /> and <Profiles /> back in
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
